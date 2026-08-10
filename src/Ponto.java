af9be2aeba165346a766aec2427d88c4178e02dd
Amanda Steglich - Looping4 <looping4.contato@gmail.com>
2026-08-10 17:19:56 -0300

    Enviando para o gitHub

---
+++ src/Ponto.java
@@ -0,0 +1,39 @@
+public class Ponto {
+    private double x;
+    private double y;
+
+    public Ponto() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Ponto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void mover(double novoX, double novoY) {
+        this.x = novoX;
+        this.y = novoY;
+    }
+
+    public String toString() {
+        return this.x + ", " + this.y;
+    }
+}
